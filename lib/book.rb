a58c9b1759ac08d9e61bc251d80b9df1e7ac4c6e
Jacob Berman <jacobberman1995@gmail.com>
2018-08-29 16:34:18 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -10,21 +10,21 @@ class Book
   def title
     @title
   end 
-  # setter method for author
-  def author=(author)
-    @author = author
-  end 
-  # getter method for author
-  def author
-    @author
-  end 
-  # setter method for pagecount
-  def page_count=(num)
-    @page_count = num
-  end 
-  #getter method for pagecount
-  def page_count
-    @page_count
-  end 
+  # # setter method for author
+  # def author=(author)
+  #   @author = author
+  # end 
+  # # getter method for author
+  # def author
+  #   @author
+  # end 
+  # # setter method for pagecount
+  # def page_count=(num)
+  #   @page_count = num
+  # end 
+  # #getter method for pagecount
+  # def page_count
+  #   @page_count
+  # end 
   
 end 

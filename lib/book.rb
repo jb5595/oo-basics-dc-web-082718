badb57f60d725eb7e227ad85e99f152100258cec
Jacob Berman <jacobberman1995@gmail.com>
2018-08-29 16:31:45 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -4,7 +4,13 @@ class Book
   def initialize(title)
     @title = title
   end
+  # Getter method for title
   def title
     @title
   end 
+  # setter method for author
+  def author=(author)
+    @author = author
+  end 
+  
 end 

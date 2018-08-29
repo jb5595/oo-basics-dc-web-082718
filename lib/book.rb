f4d365ff8ad5857c8be975a0bcb6b0d91c13b28e
Jacob Berman <jacobberman1995@gmail.com>
2018-08-29 16:35:09 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,14 +2,13 @@
 
 class Book
     attr_accessor :genre, :author, :page_count
+    attr_reader :title
 
   def initialize(title)
     @title = title
   end
   # Getter method for title
-  def title
-    @title
-  end 
+
 
   
 end 
